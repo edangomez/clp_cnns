fece82d7d369c914ad0745df6590e4749603696a
edangomez <47018181+edangomez@users.noreply.github.com>
2024-02-12 08:54:32 -0500

    util function file added

---
+++ utils/func.py
@@ -0,0 +1,20 @@
+import torch
+
+
+def check_accuracy(loader, model, device='gpu'):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+    model.train()
+    return num_correct / num_samples  
