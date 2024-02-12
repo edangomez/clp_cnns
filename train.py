a95d2c9ce1f2c5d679b098cd072c79c6b8aaaafe
edangomez <47018181+edangomez@users.noreply.github.com>
2024-02-12 08:55:06 -0500

    util function import

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from torchvision.transforms import transforms
 from tqdm import tqdm
 from core.evaluate import AverageMeter
 from core.trainer import Trainer
+from utils.func import check_accuracy
 import pandas as pd
 import os
 
@@ -24,7 +25,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Hyperparameters
 # TODO: Define the hyperparameters you want to use for training
 batch_size = 8
-num_epochs = 1
+num_epochs = 10
 learning_rate = 3e-4
 num_classes = 102
 
@@ -60,29 +61,11 @@ trainer = Trainer(
     model_dir=MODEL_DIR,
 )
 
-trainer.train()
-
-def check_accuracy(loader, model):
-    num_correct = 0
-    num_samples = 0
-    model.eval()
-
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = y.to(device=device)
-
-            scores = model(x)
-            _, predictions = scores.max(1)
-            num_correct += (predictions == y).sum()
-            num_samples += predictions.size(0)
-
-    model.train()
-    return num_correct / num_samples         
+trainer.train()       
 
 
-print(check_accuracy(train_loader, trainer.model)*100)
-print(check_accuracy(val_loader, trainer.model)*100)
+print(check_accuracy(train_loader, trainer.model, device)*100)
+print(check_accuracy(val_loader, trainer.model, device)*100)
 
 # if __name__ == "__main__": 
 #     check_accuracy()
